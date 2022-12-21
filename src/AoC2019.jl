6feacd8ae4ece07be8ba8be58da08b81de07fb02
Gabriel Wu <wuzihua@pku.edu.cn>
2022-12-21 16:48:44 +0800

    2019 Day 09

--- src/AoC2019.jl
+++ src/AoC2019.jl
@@ -15,37 +15,43 @@ function OpCode(code::Int)
     OpCode(op, modes)
 end
 
-const OP_LEN = [4, 4, 2, 2, 3, 3, 4, 4]
+const OP_LEN = [4, 4, 2, 2, 3, 3, 4, 4, 2]
 
-function exec!(mem::AbstractVector{Int}; inputs::AbstractVector{Int}=Int[], auto=true)
+function parse_arg(mem, raw, mode, relative_base)
+    if mode == 0
+        return mem[raw]
+    elseif mode == 1
+        return raw
+    elseif mode == 2
+        return mem[raw+relative_base]
+    end
+end
+
+function exec!(mem::AbstractVector{Int}; inputs::AbstractVector{Int}=Int[], auto=true, maximum_size=100000)
     n = length(mem)
-    mem = OffsetArray(mem, 0:n-1)
+    append!(mem, zeros(Int, maximum_size - n))
+    mem = OffsetArray(mem, 0:maximum_size-1)
     ptr = 0
     input_ptr = 1
+    relative_base = 0
     outputs = Int[]
     while true
         opcode = OpCode(mem[ptr])
         if opcode.op == 1
             src1, src2, target = mem[ptr+1:ptr+3]
-            if opcode.modes[1] == 0
-                src1 = mem[src1]
-            end
-            if opcode.modes[2] == 0
-                src2 = mem[src2]
-            end
+            src1 = parse_arg(mem, src1, opcode.modes[1], relative_base)
+            src2 = parse_arg(mem, src2, opcode.modes[2], relative_base)
 
-            @assert opcode.modes[3] == 0
+            @assert opcode.modes[3] != 1
+            target += (opcode.modes[3] == 2 ? relative_base : 0)
             mem[target] = src1 + src2
         elseif opcode.op == 2
             src1, src2, target = mem[ptr+1:ptr+3]
-            if opcode.modes[1] == 0
-                src1 = mem[src1]
-            end
-            if opcode.modes[2] == 0
-                src2 = mem[src2]
-            end
+            src1 = parse_arg(mem, src1, opcode.modes[1], relative_base)
+            src2 = parse_arg(mem, src2, opcode.modes[2], relative_base)
 
-            @assert opcode.modes[3] == 0
+            @assert opcode.modes[3] != 1
+            target += (opcode.modes[3] == 2 ? relative_base : 0)
             mem[target] = src1 * src2
         elseif opcode.op == 3
             target = mem[ptr+1]
@@ -57,13 +63,12 @@ function exec!(mem::AbstractVector{Int}; inputs::AbstractVector{Int}=Int[], auto
                 value = parse(Int, readline())
             end
 
-            @assert opcode.modes[1] == 0
+            @assert opcode.modes[1] != 1
+            target += (opcode.modes[1] == 2 ? relative_base : 0)
             mem[target] = value
         elseif opcode.op == 4
             src = mem[ptr+1]
-            if opcode.modes[1] == 0
-                src = mem[src]
-            end
+            src = parse_arg(mem, src, opcode.modes[1], relative_base)
 
             if auto
                 push!(outputs, src)
@@ -72,50 +77,42 @@ function exec!(mem::AbstractVector{Int}; inputs::AbstractVector{Int}=Int[], auto
             end
         elseif opcode.op == 5
             src, target = mem[ptr+1:ptr+2]
-            if opcode.modes[1] == 0
-                src = mem[src]
-            end
-            if opcode.modes[2] == 0
-                target = mem[target]
-            end
+            src = parse_arg(mem, src, opcode.modes[1], relative_base)
+            target = parse_arg(mem, target, opcode.modes[2], relative_base)
+
             if src != 0
                 ptr = target
                 continue
             end
         elseif opcode.op == 6
             src, target = mem[ptr+1:ptr+2]
-            if opcode.modes[1] == 0
-                src = mem[src]
-            end
-            if opcode.modes[2] == 0
-                target = mem[target]
-            end
+            src = parse_arg(mem, src, opcode.modes[1], relative_base)
+            target = parse_arg(mem, target, opcode.modes[2], relative_base)
+
             if src == 0
                 ptr = target
                 continue
             end
         elseif opcode.op == 7
             src1, src2, target = mem[ptr+1:ptr+3]
-            if opcode.modes[1] == 0
-                src1 = mem[src1]
-            end
-            if opcode.modes[2] == 0
-                src2 = mem[src2]
-            end
+            src1 = parse_arg(mem, src1, opcode.modes[1], relative_base)
+            src2 = parse_arg(mem, src2, opcode.modes[2], relative_base)
 
-            @assert opcode.modes[3] == 0
+            @assert opcode.modes[3] != 1
+            target += (opcode.modes[3] == 2 ? relative_base : 0)
             mem[target] = src1 < src2 ? 1 : 0
         elseif opcode.op == 8
             src1, src2, target = mem[ptr+1:ptr+3]
-            if opcode.modes[1] == 0
-                src1 = mem[src1]
-            end
-            if opcode.modes[2] == 0
-                src2 = mem[src2]
-            end
+            src1 = parse_arg(mem, src1, opcode.modes[1], relative_base)
+            src2 = parse_arg(mem, src2, opcode.modes[2], relative_base)
 
-            @assert opcode.modes[3] == 0
+            @assert opcode.modes[3] != 1
+            target += (opcode.modes[3] == 2 ? relative_base : 0)
             mem[target] = src1 == src2 ? 1 : 0
+        elseif opcode.op == 9
+            src = mem[ptr+1]
+            src = parse_arg(mem, src, opcode.modes[1], relative_base)
+            relative_base += src
         elseif opcode.op == 99
             break
         end
