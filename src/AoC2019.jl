14ebc3dbf6480a46d6ea97b73a09e5f53ab462f3
Gabriel Wu <wuzihua@pku.edu.cn>
2022-12-21 16:34:26 +0800

    2019 Day 05

--- src/AoC2019.jl
+++ src/AoC2019.jl
@@ -4,23 +4,126 @@ using ..AdventOfCode
 
 export exec!
 
-function exec!(mem::AbstractVector{Int})
+struct OpCode
+    op::Int
+    modes::NTuple{3,Int}
+end
+
+function OpCode(code::Int)
+    op = code % 100
+    modes = (code % 1000 ÷ 100, code % 10000 ÷ 1000, code ÷ 10000)
+    OpCode(op, modes)
+end
+
+const OP_LEN = [4, 4, 2, 2, 3, 3, 4, 4]
+
+function exec!(mem::AbstractVector{Int}; inputs::AbstractVector{Int}=Int[], auto=true)
     n = length(mem)
     mem = OffsetArray(mem, 0:n-1)
     ptr = 0
+    input_ptr = 1
+    outputs = Int[]
     while true
-        if mem[ptr] == 1
-            arg1, arg2, target = mem[ptr+1:ptr+3]
-            mem[target] = mem[arg1] + mem[arg2]
-            ptr += 4
-        elseif mem[ptr] == 2
-            arg1, arg2, target = mem[ptr+1:ptr+3]
-            mem[target] = mem[arg1] * mem[arg2]
-            ptr += 4
-        elseif mem[ptr] == 99
+        opcode = OpCode(mem[ptr])
+        if opcode.op == 1
+            src1, src2, target = mem[ptr+1:ptr+3]
+            if opcode.modes[1] == 0
+                src1 = mem[src1]
+            end
+            if opcode.modes[2] == 0
+                src2 = mem[src2]
+            end
+
+            @assert opcode.modes[3] == 0
+            mem[target] = src1 + src2
+        elseif opcode.op == 2
+            src1, src2, target = mem[ptr+1:ptr+3]
+            if opcode.modes[1] == 0
+                src1 = mem[src1]
+            end
+            if opcode.modes[2] == 0
+                src2 = mem[src2]
+            end
+
+            @assert opcode.modes[3] == 0
+            mem[target] = src1 * src2
+        elseif opcode.op == 3
+            target = mem[ptr+1]
+
+            if auto
+                value = inputs[input_ptr]
+                input_ptr += 1
+            else
+                value = parse(Int, readline())
+            end
+
+            @assert opcode.modes[1] == 0
+            mem[target] = value
+        elseif opcode.op == 4
+            src = mem[ptr+1]
+            if opcode.modes[1] == 0
+                src = mem[src]
+            end
+
+            if auto
+                push!(outputs, src)
+            else
+                print(src)
+            end
+        elseif opcode.op == 5
+            src, target = mem[ptr+1:ptr+2]
+            if opcode.modes[1] == 0
+                src = mem[src]
+            end
+            if opcode.modes[2] == 0
+                target = mem[target]
+            end
+            if src != 0
+                ptr = target
+                continue
+            end
+        elseif opcode.op == 6
+            src, target = mem[ptr+1:ptr+2]
+            if opcode.modes[1] == 0
+                src = mem[src]
+            end
+            if opcode.modes[2] == 0
+                target = mem[target]
+            end
+            if src == 0
+                ptr = target
+                continue
+            end
+        elseif opcode.op == 7
+            src1, src2, target = mem[ptr+1:ptr+3]
+            if opcode.modes[1] == 0
+                src1 = mem[src1]
+            end
+            if opcode.modes[2] == 0
+                src2 = mem[src2]
+            end
+
+            @assert opcode.modes[3] == 0
+            mem[target] = src1 < src2 ? 1 : 0
+        elseif opcode.op == 8
+            src1, src2, target = mem[ptr+1:ptr+3]
+            if opcode.modes[1] == 0
+                src1 = mem[src1]
+            end
+            if opcode.modes[2] == 0
+                src2 = mem[src2]
+            end
+
+            @assert opcode.modes[3] == 0
+            mem[target] = src1 == src2 ? 1 : 0
+        elseif opcode.op == 99
             break
         end
+
+        ptr += OP_LEN[opcode.op]
     end
+
+    return outputs
 end
 
 end
